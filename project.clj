86d6bff2f04c5fb3d9e1afdfd1e7d5c0b04513c3
HIRAM MADELAINE <hiram.madelaine@gmail.com>
2015-06-29 14:16:06 +0200

    bump om-inputs, to test stepper

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [sablono "0.3.4"]
                  [org.omcljs/om "0.8.8"]
-                 [om-inputs "0.3.8"]
+                 [om-inputs "0.3.9-SNAPSHOT"]
                  ]
 
   :plugins [[lein-cljsbuild "1.0.5"]
