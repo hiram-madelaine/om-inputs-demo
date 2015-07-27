0aa8af120b3517045eb9d675dd5548ffca09269f
HIRAM MADELAINE <hiram.madelaine@gmail.com>
2015-07-27 11:11:59 +0200

    ADD grunt for css

---
+++ GruntFile.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        less: {
+              css: {
+                src: ['src/css/app.less'],
+                dest: 'target/app-less.css',
+              }
+            },
+         autoprefixer: {
+             options: {
+
+             },
+              single_file: {
+                   options: {
+                     // Target-specific options go here.
+                   },
+                   src: 'target/app-less.css',
+                   dest: 'resources/public/css/style.css'
+                 }
+         },
+        concat: {
+            css: {
+                src: ['resources/public/css/bootstrap.min.css',
+                        'resources/public/css/forms.css',
+                        'resources/public/css/datepicker.css',
+                        'resources/public/css/popupdatepicker.css',
+                        'resources/public/css/style.css'],
+                dest: 'resources/public/css/release.css'
+            }},
+        cssmin: {
+            css:{
+                src: 'resources/public/css/release.css',
+                dest: 'resources/public/css/release.min.css'
+            }
+        },
+        watch: {
+            files: ['src/css/app.less'],
+            tasks: ['less', 'autoprefixer']
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.registerTask('default', ['less', 'autoprefixer','concat','cssmin' ]);
+};
