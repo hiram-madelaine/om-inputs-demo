f581c3a28cc76306b9f51a81f12fde99ecddcb1b
HIRAM MADELAINE <hiram.madelaine@gmail.com>
2015-08-07 12:52:29 +0200

    Add core.async in compilation cache

--- src/cljs/om_inputs_demo/eval_helper.cljs
+++ src/cljs/om_inputs_demo/eval_helper.cljs
@@ -1,8 +1,10 @@
 (ns ^:figwheel-always om-inputs-demo.eval-helper
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
             [om-inputs.core :as in :refer [make-input-comp]]
-            [schema.core :as s]))
+            [schema.core :as s]
+            [cljs.core.async :refer [put! chan <!]]))
 
 
 (defn build-comp
