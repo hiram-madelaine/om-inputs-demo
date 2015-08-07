6eb2cd805a3b1cfcf99cbc4faedf0c2d54c5a8c9
HIRAM MADELAINE <hiram.madelaine@gmail.com>
2015-08-07 12:51:30 +0200

    Get rid of dependency

--- project.clj
+++ project.clj
@@ -9,8 +9,7 @@
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [sablono "0.3.4"]
                  [org.omcljs/om "0.8.8"]
-                 [om-inputs "0.3.9-SNAPSHOT"]
-                 [fipp "0.6.2"]]
+                 [om-inputs "0.3.9-SNAPSHOT"]]
 
   :plugins [[lein-cljsbuild "1.0.5"]
             [lein-figwheel "0.3.7"]]
